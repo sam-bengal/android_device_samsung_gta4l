b46c35337261de0ac98bee8461bac5adde284ea6
itsnouralawad <itsnouralawad@gmail.com>
2023-10-28 22:26:32 +0000

    gta4l : Inherit Common And add support for gta4ltur

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -6,5 +6,11 @@
 
 DEVICE_PATH := device/samsung/gta4l
 
+# Inherit from SM6115-common
+-include device/samsung/SM6115-common/BoardConfigCommon.mk
+
+# Asserts
+TARGET_OTA_ASSERT_DEVICE := gta4l,gta4ltur
+
 # Inherit the proprietary files
 include vendor/samsung/gta4l/BoardConfigVendor.mk
