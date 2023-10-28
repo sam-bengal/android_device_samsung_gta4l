c833452e8fe9dfcebaa6e036aa11c28456d42685
itsnouralawad <itsnouralawad@gmail.com>
2023-10-28 22:45:36 +0000

    gta4l: Remove Screen density since its defined in COMMON TREE

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -9,9 +9,6 @@ DEVICE_PATH := device/samsung/gta4l
 # Inherit from SM6115-common
 -include device/samsung/SM6115-common/BoardConfigCommon.mk
 
-# Display
-TARGET_SCREEN_DENSITY := 224
-
 # Asserts
 TARGET_OTA_ASSERT_DEVICE := gta4l,gta4ltur
 
