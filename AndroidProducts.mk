1dab45aed614b51ff058a80cc6bc7a9c8c15a18a
itsnouralawad <itsnouralawad@gmail.com>
2023-10-28 22:12:39 +0000

    gta4l : Initial tree Gen

---
+++ AndroidProducts.mk
@@ -0,0 +1,13 @@
+#
+# Copyright (C) 2023 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/lineage_gta4l.mk
+
+COMMON_LUNCH_CHOICES := \
+    lineage_gta4l-user \
+    lineage_gta4l-userdebug \
+    lineage_gta4l-eng
